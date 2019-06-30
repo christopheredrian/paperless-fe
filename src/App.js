528ce5ef85c5de935e677065d8ea7d5fcabbba56
Christopher Espiritu <christopheredrian@gmail.com>
2019-06-29 20:44:49 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// in src/App.js
+import React from 'react';
+import {Admin, Resource} from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+import {UserList} from './components/users/UserList';
+
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+
+const App = () => (
+    <Admin dataProvider={dataProvider}>
+        <Resource name="users" list={UserList}/>
+    </Admin>
+);
+
+export default App;
